2bbfa89d109ccb5b7cc358adf9270460aa927c13
fart-source <64970074+fart-source@users.noreply.github.com>
2020-08-20 02:30:44 -0400

    Add files via upload

---
+++ Desktop Web Shortcut Creator.bat
@@ -0,0 +1,24 @@
+@echo off
+color 02
+title "Desktop Web Shortcut Creator"
+
+:check1
+
+cls
+echo URL:
+set /p WebAddress=">"
+if "%WebAddress%"=="" goto :check1
+
+:check2
+
+cls
+echo NAME:
+set /p ShortCutName=">"
+if "%ShortCutName%"=="" goto :check2
+
+echo @echo off>>Temporary_Shortcut.txt
+echo color 02>>Temporary_Shortcut.txt
+echo start "microsoft edge" %WebAddress%>>Temporary_Shortcut.txt
+echo exit>>Temporary_Shortcut.txt
+ren Temporary_Shortcut.txt %ShortCutName%.bat
+exit
